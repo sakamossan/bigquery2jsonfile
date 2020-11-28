59994a82e70e666b3d12f92d22a97036768b2fde
sakamossan <mild7caloriemategreentea@gmail.com>
2020-11-28 11:25:06 +0900

    add gcsSaveOptions

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const fs = require('fs');
-const { BigQuery } = require('@google-cloud/bigquery');
-const { Storage } = require('@google-cloud/storage');
-const camelcaseKeys = require('camelcase-keys');
+const fs = require("fs");
+const { BigQuery } = require("@google-cloud/bigquery");
+const { Storage } = require("@google-cloud/storage");
+const camelcaseKeys = require("camelcase-keys");
 
 class BigQuery2JsonFile {
   constructor(options) {
     this.sql = options.sql;
     this.output = options.output;
     this.camelcase = options.camelcase;
-    this.transformFunction = options.transform || (a => a);  // option used when called from nodejs program
+    this.gcsSaveOptions = options.gcsSaveOptions || {};
 
-    const {project_id, scope, service_account_credential_file} = options;
+    // options used when called from nodejs program
+    this.transformFunction = options.transform || ((a) => a);
+
+    const { project_id, scope, service_account_credential_file } = options;
     this.bigquery = new BigQuery({
       projectId: project_id,
       credentials: require(service_account_credential_file),
@@ -18,52 +21,53 @@ class BigQuery2JsonFile {
     });
     this.storage = new Storage({
       projectId: project_id,
-      keyFilename: service_account_credential_file
-    })
+      keyFilename: service_account_credential_file,
+    });
   }
 
   async extract() {
-    const sqlString = await (new Promise((resolve, reject) =>
-      fs.readFile(
-        this.sql,
-        (err, data) => err ? reject(err) : resolve(data.toString())
+    const sqlString = await new Promise((resolve, reject) =>
+      fs.readFile(this.sql, (err, data) =>
+        err ? reject(err) : resolve(data.toString())
       )
-    ));
+    );
     const [result] = await this.bigquery.query(sqlString);
     return result;
   }
 
-  transform (a) {
+  transform(a) {
     a = this.camelcase ? camelcaseKeys(a) : a;
-    return this.transformFunction(a)
+    return this.transformFunction(a);
   }
 
   load(data) {
-    const content = JSON.stringify(data)
+    const content = JSON.stringify(data);
     if (!this.output) {
       process.stdout.write(content);
-    } else if (this.output.startsWith('gs://')) {
+    } else if (this.output.startsWith("gs://")) {
       const [bucketName, ...keys] = this.output.replace("gs://", "").split("/");
+      // https://googleapis.dev/nodejs/storage/latest/global.html#CreateWriteStreamOptions
+      const options = {
+        contentType: "application/json",
+        ...this.gcsSaveOptions,
+      };
       return this.storage
         .bucket(bucketName)
         .file(keys.join("/"))
-        .save(content, {
-          contentType: "application/json",
-        })
+        .save(content, options);
     } else {
       return new Promise((resolve, reject) =>
-        fs.writeFile(
-          this.output,
-          content,
-          err => err ? reject(err) : resolve())
-      )
+        fs.writeFile(this.output, content, (err) =>
+          err ? reject(err) : resolve()
+        )
+      );
     }
   }
 
   run() {
     return this.extract()
-      .then(data => this.transform(data))
-      .then(data => this.load(data))
+      .then((data) => this.transform(data))
+      .then((data) => this.load(data));
   }
 }
 
